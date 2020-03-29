9e08018a0a266df3bf46f30f6cbb40c176d0487f
Maksim Kirilko <maksimkirilko@gmail.com>
2020-03-29 22:22:23 +0300

    feat: add process.stdin as stream

--- caesar-cipher-cli/README.md
+++ caesar-cipher-cli/README.md
@@ -1,7 +1,13 @@
 ### Task 1. Caesar cipher CLI tool
 
 Run:
-node caesar-cipher-cli -i './input.txt' -o './output.txt' -s 7 -a encode
+```node caesar-cipher-cli -i './input.txt' -o './output.txt' -s 7 -a encode```
+
+Run without input: ```node caesar-cipher-cli -o './output.txt' -s 1 -a encode```
+
+Run without output: ```node caesar-cipher-cli -i './input.txt' -s 1 -a encode```
+
+Run without input and output: ```node caesar-cipher-cli -s 1 -a encode```
 
 CLI tool should accept 4 options (short alias and full name):
 
--- caesar-cipher-cli/index.js
+++ caesar-cipher-cli/index.js
@@ -2,11 +2,9 @@ const process = require('process');
 const fs = require('fs');
 const path = require('path');
 const { pipeline } = require('stream');
-const readline = require('readline');
 
 const { CaesarCipherTransform } = require('./transform-stream');
-const { action, shift, output } = require('./parse');
-let { input } = require('./parse');
+const { action, input, output, shift } = require('./parse');
 
 if (Number.isNaN(shift)) {
   console.error('Shift parameter should be provided as number');
@@ -20,28 +18,12 @@ if (typeof action !== 'string') {
   process.exit(1);
 }
 
-if (!input) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  rl.question(
-    `Input file is not provided so enter string to ${action} manually: `,
-    inputString => {
-      input = inputString;
-      console.log('program', { action, shift, input, output });
-      rl.close();
-    }
-  );
-}
-
 const transformStream = new CaesarCipherTransform({ shift, action });
 
 pipeline(
-  fs.createReadStream(path.resolve(__dirname, input)),
+  Boolean(input) ? fs.createReadStream(path.resolve(__dirname, input)) : process.stdin,
   transformStream,
-  fs.createWriteStream(path.resolve(__dirname, output)),
+  Boolean(output) ? fs.createWriteStream(path.resolve(__dirname, output)) : process.stdout,
   pipelineCallback
 );
 
